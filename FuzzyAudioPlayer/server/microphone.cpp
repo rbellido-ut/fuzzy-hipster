818ebb7bb7e95359a02f524f56e27a61f559b38f
Ronald Bellido <rbellido54@gmail.com>
2013-03-30 01:50:55 -0700

    started microphone implementation

--- FuzzyAudioPlayer/server/microphone.cpp
+++ FuzzyAudioPlayer/server/microphone.cpp
@@ -5,11 +5,24 @@ bool MicChat::onStart()
 	return true;
 }
 
-bool MicChat::OnProcessSamples(const sf::Int16 * Samples, std::size_t SamplesCount)
+bool MicChat::onProcessSamples(const sf::Int16 * Samples, std::size_t SamplesCount)
 {
+	//send over network
 	return true;
 }
 
 void MicChat::onStop()
 {
+}
+
+int MicChat::micSession()
+{
+	if (sf::SoundRecorder::isAvailable() == false)
+	{
+		std::cout << "Sorry, audio capture is not supported by your system" << std::endl;
+		//perhaps send this same message to the client, or some error message
+		return 0;
+	}
+
+	//initialize sample rate
 }
--- FuzzyAudioPlayer/server/microphone.h
+++ FuzzyAudioPlayer/server/microphone.h
@@ -1,15 +1,18 @@
 #ifndef __MICROPHONE_H
 #define __MICROPHONE_H
 
+#include "SFML\Audio.hpp"
 #include "utils.h"
 #include "server_net.h"
-#include "SFML\Audio.hpp"
+
 
 class MicChat : public sf::SoundRecorder
 {
-	virtual bool onStart();
-	virtual bool OnProcessSamples(const sf::Int16 * Samples, std::size_t SamplesCount);
-	virtual void onStop();
+	virtual bool onStart(); //optional to implement
+	virtual bool onProcessSamples(const sf::Int16 * Samples, std::size_t SamplesCount);
+	virtual void onStop(); //optional to implement
+
+	int micSession();
 };
 
 #endif
